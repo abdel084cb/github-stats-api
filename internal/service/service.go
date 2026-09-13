36291b68800b3d486a0c3956cd5ca9876d0f171f
abdel <abdel@example.com>
2026-09-13 20:59:15 +0200

    feat: add GitHub username validation on service

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"context"
+	"regexp"
+
+	"github.com/abdelbassat/github-stats-api/internal/apperrors"
 	"github.com/abdelbassat/github-stats-api/internal/githubclient"
+	"github.com/abdelbassat/github-stats-api/internal/model"
 	"github.com/abdelbassat/github-stats-api/internal/repository"
 )
 
@@ -15,4 +20,31 @@ func NewService(repository *repository.Repository, githubClient *githubclient.Cl
 		repository:   repository,
 		githubClient: githubClient,
 	}
+}
+
+const maxUsernameLength = 39
+
+var usernamePattern *regexp.Regexp = regexp.MustCompile(
+	`^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$`,
+)
+
+func isValidUsername(username string) bool {
+	if username == "" {
+		return false
+	}
+
+	if len(username) > maxUsernameLength {
+		return false
+	}
+
+	return usernamePattern.MatchString(username)
+}
+
+func (s *Service) GetStatsByUsername(ctx context.Context, username string) (stats model.Stats, err error) {
+
+	if !isValidUsername(username) {
+		return model.Stats{}, apperrors.ErrInvalidUsername
+	}
+
+	return model.Stats{}, nil
 }
