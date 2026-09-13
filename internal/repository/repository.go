59cc55f58e46fbb96477dde70b323aa577a4db9c
abdel <abdel@example.com>
2026-09-13 20:40:50 +0200

    feat: added function in package repository to get stats using the username, get stats by username

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,7 +1,11 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"time"
+
+	"github.com/abdelbassat/github-stats-api/internal/model"
 )
 
 type Repository struct {
@@ -27,4 +31,43 @@ func (r *Repository) CreateDatabase(db *sql.DB) error {
 	_, err := db.Exec(query)
 
 	return err
+}
+
+func (r *Repository) GetStatsByUsername(ctx context.Context, username string) (model.Stats, error) {
+	const query = `
+		SELECT * FROM stats WHERE username = ?;
+	`
+
+	row := r.db.QueryRowContext(ctx, query, username)
+
+	if row.Err() != nil {
+		return model.Stats{}, row.Err()
+	}
+
+	var result model.Stats
+	var cachedAtText string
+	var topLanguage sql.NullString
+
+	err := row.Scan(
+		&result.Username,
+		&result.TotalRepos,
+		&result.TotalStars,
+		&topLanguage,
+		&cachedAtText,
+	)
+	if err != nil {
+		return model.Stats{}, err
+	}
+
+	result.CachedAt, err = time.Parse(time.RFC3339, cachedAtText)
+	if err != nil {
+		return model.Stats{}, err
+	}
+
+	if topLanguage.Valid {
+		language := topLanguage.String
+		result.TopLanguage = &language
+	}
+
+	return result, nil
 }
